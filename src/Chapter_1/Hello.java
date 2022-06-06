ff8ab70bb18a59f290237c23cc82dc8011f0deda
Maksim <maksim.medvedchuk89@gmail.com>
2022-06-06 21:57:19 +0300

    first commit

---
+++ src/Chapter_1/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+////Приветствовать любого пользователя при вводе его имени через командную строку
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name and to press <Enter>");
+        String string = scanner.next();
+        System.out.println("Hello " + string);
+    }
+}
