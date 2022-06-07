423ff1420f17686589530c3a4e5658b0a7a97b7f
Maksim <maksim.medvedchuk89@gmail.com>
2022-06-07 20:22:07 +0300

    Gitignore

--- src/Chapter_1/EnterPassword.java
+++ src/Chapter_1/EnterPassword.java
@@ -1,2 +1,25 @@
-package Chapter_1;public class EnterPassword {
+package Chapter_1;
+import java.util.Scanner;
+//Ввести пароль из командной строки и сравнить его со строкой-образцом.
+
+public class EnterPassword {
+    static String password = "123456";
+    static String yourPassword;
+    static String ImputYourPassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter password ");
+        yourPassword = scanner.nextLine();
+        System.out.println("Your Password " + yourPassword);
+        return yourPassword;
+    }
+    static void ComparePasswords() {
+        if (password.equals(yourPassword))
+            System.out.println("Your Password is true");
+        else
+            System.out.println("Your Password is false");
+    }
+    public static void main(String[] args) {
+        ImputYourPassword();
+        ComparePasswords();
+    }
 }
