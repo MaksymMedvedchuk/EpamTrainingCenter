fd8465e0c255d7cbc4fe3ad36bf3b95d7b746f9e
Maksim <maksim.medvedchuk89@gmail.com>
2022-06-09 20:36:49 +0300

    Task5a

--- src/сhapter1/varianta/task5/CountsSumsArgs.java
+++ src/сhapter1/varianta/task5/CountsSumsArgs.java
@@ -10,7 +10,8 @@ public class CountsSumsArgs {
 
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; true; i++) {
-
+            int NUN1 = scanner.nextInt();
+            int NUN2 = scanner.nextInt();
 
             System.out.println("Sum= " + (NUN1 + NUN2));
 
