ba8e14fec9a66fcd0a1396cabef92c703acef7ad
Maksim <maksim.medvedchuk89@gmail.com>
2022-08-19 18:25:45 +0300

    New tasks and edits

--- src/chapter2/varianta/task4/MinDifferentDigitsNumbersPrinter.java
+++ src/chapter2/varianta/task4/MinDifferentDigitsNumbersPrinter.java
@@ -1,9 +1,23 @@
 package chapter2.varianta.task4;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MinDifferentDigitsNumbersPrinter {
-    public void printNumbers(){
+    public void printNumbers(String[] numbers) {
+       // Arrays.stream(numbers)
+                //.filter(x -> isDifferent(x))
 
 
+    }
+
+    private int isDifferent(String input) { //чому int в нас же differentDigits це char
+        Set<Character> differentDigits = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            differentDigits.add(input.charAt(i));
+        }
+        return differentDigits.size();
     }
 
 }
